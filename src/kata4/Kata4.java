3ed7057949ca612a5d47e1cc7cf598bccedccab3
usuario <usuario@10.22.143.9>
2015-11-05 09:14:39 +0000

    Version 2

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,6 +1,8 @@
 package kata4;
 
-import java.io.File;
+import java.io.*;
+import java.util.Date;
+
 
 /**
  *
@@ -11,7 +13,7 @@ public class Kata4 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         String path ="C:\\Users\\usuario\\Documents";
          File file = new File(path);
         String[] fileList= file.list();
@@ -20,8 +22,26 @@ public class Kata4 {
             //System.out.print(fileList1);
         }
         
-        print(file.listFiles(),"");
+        //print(file.listFiles(),"");
+        String from ="C:\\Users\\Public\\Pictures\\Sample Pictures\\Chrysanthemum.jpg";
+        String to="C:\\Users\\Public\\Pictures\\Sample Pictures\\Chrysanthemum(1).jpg";
         
+        BufferedInputStream  input=new BufferedInputStream(new FileInputStream(new File(from)));
+        BufferedOutputStream  output=new BufferedOutputStream(new FileOutputStream(new File(to)));
+        Date date = new Date();
+        while(true){
+            int read = input.read();
+            if(read<0){
+                break;
+            }else{
+                output.write(read);
+            }
+           
+        }
+        System.out.print(new Date().getTime() - date.getTime() + "msg");
+        input.close();
+        output.flush();
+        output.close();
     }
 
     private static void print(File[] listFiles, String ident) {
